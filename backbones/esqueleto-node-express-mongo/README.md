dc1a9653819baca220fd14546c515267fc688b8f
Nestor Villalobos <nestorvc@gmail.com>
2014-02-04 14:24:09 -0600

    bugs-free ready

--- backbones/esqueleto-node-express-mongo/README.md
+++ backbones/esqueleto-node-express-mongo/README.md
@@ -23,3 +23,14 @@
 The Express framework uses the NODE_ENV environment variable to determine some behaviors related to caching. If you’re using Express, set a config var with this value:
 
 $ heroku config:set NODE_ENV=production
+
+## "Error: failed to connect to [localhost:XXXXX]"
+If you are getting this error while running on local, remember to start your MongoDB from Terminal with the following commands:
+
+    mongod
+    mongo
+
+If you don't have MongoDB installed in your local environment, follow this instructions: http://docs.mongodb.org/manual/tutorial/install-mongodb-on-os-x/
+
+    
+
--- backbones/esqueleto-node-express-mongo/config/routes.js
+++ backbones/esqueleto-node-express-mongo/config/routes.js
@@ -17,8 +17,8 @@
  // -----------------------
 
  var passportLocalOptions = {
-   successRedirect: '/ejemplos',
-   failureRedirect: '/',
+    successRedirect: '/ejemplos',
+    failureRedirect: '/',
  }
 
  // -----------------------
@@ -27,15 +27,15 @@
 
  module.exports = function(app) {
 
-   //Ejemplo  
-   app.get('/', ejemploController.login);
-   app.post('/auth/login', passport.authenticate('local', passportLocalOptions));
-   app.get('/logout', auth.requiresLogin, ejemploController.logout);
-   app.get('/ejemplos', auth.requiresLogin, ejemploController.index);
-   app.get('/ejemplos/new', auth.requiresLogin, ejemploController.new);
-   app.get('/ejemplos/:objectId', auth.requiresLogin, ejemploController.readIt);
-   app.post('/ejemplos/create', auth.requiresLogin, ejemploController.createIt);
-   app.post('/ejemplos/:objectId', auth.requiresLogin, ejemploController.updateIt);
-   app.post('/ejemplos/:objectId', auth.requiresLogin, ejemploController.upsertIt);
-   app.post('/ejemplos/:objectId', auth.requiresLogin, ejemploController.deleteIt;
-   }
+    //Ejemplo  
+    app.get('/', ejemploController.login);
+    app.post('/auth/login', passport.authenticate('local', passportLocalOptions));
+    app.get('/logout', auth.requiresLogin, ejemploController.logout);
+    app.get('/ejemplos', auth.requiresLogin, ejemploController.index);
+    app.get('/ejemplos/new', auth.requiresLogin, ejemploController.new);
+    app.get('/ejemplos/:objectId', auth.requiresLogin, ejemploController.readIt);
+    app.post('/ejemplos/create', auth.requiresLogin, ejemploController.createIt);
+    app.post('/ejemplos/:objectId', auth.requiresLogin, ejemploController.updateIt);
+    app.post('/ejemplos/:objectId', auth.requiresLogin, ejemploController.upsertIt);
+    app.post('/ejemplos/:objectId', auth.requiresLogin, ejemploController.deleteIt);
+ }
